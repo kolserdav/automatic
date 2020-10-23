ed0a4607ae26684c4db462ed31a136995301766d
kolserdav <serega12101983@gmail.com>
2020-10-23 22:31:23 +0700

    Before development

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <button onClick={sendMail}>Send</button>
+      {/*<button onClick={sendMail}>Send</button>*/}
     </div>
   )
 }
