ccbad53c798f1d95fc864499612a48a87a426c76
kolserdav <serega12101983@gmail.com>
2020-10-26 00:29:51 +0700

    Person block css added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,50 @@
 import Head from 'next/head'
 import classnames from 'classnames'
-import s from '../styles/Home.module.scss'
+import h from '../styles/home/Header.module.scss'
+import n from '../styles/home/Notice.module.scss'
+import s from '../styles/home/Stage.module.scss'
+import p from '../styles/home/Person.module.scss'
+import a from '../styles/home/Adv.module.scss'
+import { SvgIcon } from '@material-ui/core'
+import ReportProblemIcon from '@material-ui/icons/ReportProblem';
+import FaceIcon from '@material-ui/icons/Face'
+import StorageIcon from '@material-ui/icons/Storage'
+import DoneOutLineIcon from '@material-ui/icons/DoneOutline'
+import DevicesOtherIcon from '@material-ui/icons/DevicesOther'
+import ContactSupportIcon from '@material-ui/icons/ContactSupport'
+import DoneIcon from '@material-ui/icons/Done';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core'
+import MyApp from './_app'
+import { loadGetInitialProps } from 'next/dist/next-server/lib/utils'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#104b5d'
+    },
+    secondary: {
+      main: '#5c093e'
+    },
+    action: {
+      active: '#f78b00',
+      disabled: '#df6300'
+    },
+    error: {
+      main: '#970110'
+    }
+  }
+})
+
+const stageIcon = {
+  margin: '0 auto 0 auto',
+  fontSize: '80px',
+  marginTop: '40px'
+};
+
+const advIcon = {
+  fontSize: '80px',
+  margin: 'auto 30px auto 12px',
+};
 
 const sendMail = () => {
   fetch('api/mailer')
@@ -9,7 +53,12 @@ const sendMail = () => {
     .catch(e => console.error(e))
 }
 
-export default function Home() {
+const MyButton = (props) => {
+  return <button className={classnames(n.askButton, 'button')}>{props.text}</button>
+};
+
+export default function Home(props) {
+
 
   if (typeof window !== 'undefined') {
     window.addEventListener('resize', () => {
@@ -18,65 +67,122 @@ export default function Home() {
   }
 
   return (
-    <div className={classnames(s.container, s.column, s.center)}>
+    <ThemeProvider theme={theme}>
+    <div className={classnames('container', 'column', 'center')}>
       <Head>
         <title>Dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={classnames(s.header, s.fullScreen)}>
-        <div className={classnames(s.shadow, s.column, s.center, s.fullScreen)}>
-          <div className={classnames(s.headerText, s.centerText)}><h1>Автоматизация процессов обработки данных</h1></div>
-          <div className={classnames(s.headerDescription, s.centerText)}>Создание простых и сложных скриптов выполняющих повторяющиеся действия при работе с данными на вашем ПК или веб сервисе</div>
+      <header className={classnames(h.header)}>
+        <div className={classnames(h.shadow, 'column', 'center', 'fullScreen')}>
+          <div className={classnames(h.headerText, 'centerText')}><h1>Автоматизация процессов обработки данных</h1></div>
+          <div className={classnames(h.headerDescription, 'centerText')}>Создание простых и сложных скриптов выполняющих повторяющиеся действия при работе с данными на вашем ПК или веб сервисе</div>
         </div>
       </header>
-      <div className={classnames(s.notice, s.column, s.center, s.fullScreen)}>
-        <div className={s.line}></div>
-        <div className={classnames(s.textNotice, s.centerText)}><h1>Хотите сократить время на частых повторяющихся действиях при работе с данными, но не знаете с чего начать?</h1></div>
-        <div className={s.line}></div>
-        <button className={classnames(s.askButton, s.button)}>Консультация</button>
-      </div>
-      <main className={classnames(s.main, s.column, s.start)}>
-        <div className={classnames(s.stages, s.column)}>
-          <div className={classnames(s.column, s.center)}>
-            <div className={s.boldHeader}><h5>Как происходит процесс создания скрипта автоматизации?</h5></div>
-            <div className={classnames(s.boldDesc, s.paddingSmall)}><p>Создание компьютерных программ состоит из нескольких этапов:</p></div>
+      <main className={classnames(s.main, 'column', 'start')}>
+        <div className={classnames(n.notice, 'column', 'center', 'fullScreen')}>
+          <div className={n.line}></div>
+          <div className={classnames(n.textNotice, 'centerText')}><h1>Хотите сократить время на частых повторяющихся действиях при работе с данными, но не знаете с чего начать?</h1></div>
+          <div className={n.line}></div>
+          <div className={n.firstButton}>
+            <MyButton text="Консультация" />
           </div>
-          <div className={classnames(s.column, s.center)}>
-            <div className={classnames(s.stageCards, s.paddingSmall)}>
-              <div className={classnames(s.stageCard, s.column, s.center)}>
-                <img className={s.stageIcon} src="img/stages/1.svg"></img>
-                <div className={classnames(s.boldHeader)}><h6>Определение проблемы</h6></div>
-                <div className={classnames(s.boldDesc, s.paddingSmall)}><span>Вначале, нужно сформулировать проблему и поискать готовые решения. Если существующие средства не подходят, переходим ко второму этапу...</span></div>
+        </div>
+        <div className={classnames(s.stages, 'column')}>
+          <div className={classnames('column', 'center')}>
+            <div className={'boldHeader'}><h4>Как проходит процесс создания скрипта автоматизации?</h4></div>
+            <div className={classnames('boldDesc', 'big')}><p><u>Создание компьютерных программ состоит из нескольких этапов:</u></p></div>
+          </div>
+          <div className={classnames('column', 'center')}>
+            <div className={classnames(s.stageCards, 'paddingSmall')}>
+              <div className={classnames(s.stageCard)}>
+                <ReportProblemIcon style={stageIcon} color="error" />
+                <div className={classnames('boldHeader')}><h6>Определение проблемы</h6></div>
+                <div className={classnames('boldDesc', 'paddingSmall')}><span>Вначале, нужно сформулировать проблему и поискать готовые решения. Если существующие средства не подходят, переходим ко второму этапу...</span></div>
               </div>
-              <div className={classnames(s.stageCard, s.column, s.center)}>
-                <img className={s.stageIcon} src="img/stages/2.svg"></img>
-                <div className={classnames(s.boldHeader)}><h6>Выбор специалиста</h6></div>
-                <div className={classnames(s.boldDesc, s.paddingSmall)}><span>На этом этапе делаются заявки на выполнение задачи. Ведутся переговоры с потенциальными исполнителями. Заключается договор с подходящим специалистом.</span></div>
+              <div className={classnames(s.stageCard)}>
+                <FaceIcon style={stageIcon} color="error" />
+                <div className={classnames('boldHeader')}><h6>Выбор специалиста</h6></div>
+                <div className={classnames('boldDesc', 'paddingSmall')}><span>На этом этапе делаются заявки на выполнение задачи. Ведутся переговоры с потенциальными исполнителями. Заключается договор с подходящим специалистом.</span></div>
               </div>
-              <div className={classnames(s.stageCard, s.column, s.center)}>
-                <img className={s.stageIcon} src="img/stages/3.svg"></img>
-                <div className={classnames(s.boldHeader)}><h6>Уточнение деталей</h6></div>
-                <div className={classnames(s.boldDesc, s.paddingSmall)}><span>Перед началом разработки, описать исполнителю все значимые детали будущей программы. В процессе обсуждения, формируется и утверждается Техническое задание (ТЗ), оно будет главным документом в оценке выполнения проекта.</span></div>
+              <div className={classnames(s.stageCard)}>
+                <ContactSupportIcon style={stageIcon} color="error" />
+                <div className={classnames('boldHeader')}><h6>Уточнение деталей</h6></div>
+                <div className={classnames('boldDesc', 'paddingSmall')}><span>Перед началом разработки, описать исполнителю все значимые детали будущей программы. В процессе обсуждения, формируется и утверждается Техническое задание (ТЗ), оно будет главным документом в оценке готовности проекта.</span></div>
               </div>
-              <div className={classnames(s.stageCard, s.column, s.center)}>
-                <img className={s.stageIcon} src="img/stages/4.svg"></img>
-                <div className={classnames(s.boldHeader)}><h6>Представление данных</h6></div>
-                <div className={classnames(s.boldDesc, s.paddingSmall)}><span>Специалисту могут потребоваться образцы целевых данных. Обычно, их предоставление оговаривается ещё на этапе создания ТЗ.</span></div>
+              <div className={classnames(s.stageCard)}>
+                <StorageIcon style={stageIcon} color="error" />
+                <div className={classnames('boldHeader')}><h6>Представление данных</h6></div>
+                <div className={classnames('boldDesc', 'paddingSmall')}><span>Специалисту могут потребоваться образцы целевых данных для тестирования программы. Обычно, их предоставление оговаривается ещё на этапе создания ТЗ.</span></div>
               </div>
-              <div className={classnames(s.stageCard, s.column, s.center)}>
-                <img className={s.stageIcon} src="img/stages/5.svg"></img>
-                <div className={classnames(s.boldHeader)}><h6>Приёмка работы</h6></div>
-                <div className={classnames(s.boldDesc, s.paddingSmall)}><span>Тестирование готовой программы. В случае, если утвержденная в ТЗ функция неправильно работает, исполнитель обязан её устранить.</span></div>
+              <div className={classnames(s.stageCard)}>
+                <DoneOutLineIcon style={stageIcon} color="error" />
+                <div className={classnames('boldHeader')}><h6>Приёмка работы</h6></div>
+                <div className={classnames('boldDesc', 'paddingSmall')}><span>Тестирование готовой программы. В случае, если утвержденная в ТЗ функция, или другая производная работает неправильно, исполнитель обязан её устранить.</span></div>
               </div>
-              <div className={classnames(s.stageCard, s.column, s.center)}>
-                <img className={s.stageIcon} src="img/stages/6.svg"></img>
-                <div className={classnames(s.boldHeader)}><h6>Внедрение в процесс</h6></div>
-                <div className={classnames(s.boldDesc, s.paddingSmall)}><span>После окончания разработки и тестировании при приёмке программа встраивается в то место где она решает определённую проблему.</span></div>
+              <div className={classnames(s.stageCard)}>
+                <DevicesOtherIcon style={stageIcon} color="error" />
+                <div className={classnames('boldHeader')}><h6>Внедрение в процесс</h6></div>
+                <div className={classnames('boldDesc', 'paddingSmall')}><span>После окончания разработки и тестирования, программа встраивается в то место где она решает свою задачу.</span></div>
               </div>
+            </div>
+            <MyButton text="Отправить заявку" />
+          </div>
+        </div>
+        <div className={p.delimiter}></div>
+        <div className={classnames(p.person, 'center')}>
+          <div className={p.personDesc}>
+            <div className={p.personName}><h4>Сергей Кольмиллер</h4></div>
+            <div className={p.personSpec}><span><i>Программист</i></span></div>
+            <div className={p.personList}>
+              <li className={p.personLi}>Более 10-ти лет опыта в IT;</li>
+              <li className={p.personLi}>Более 3-х лет в программировании;</li>
+              <li className={p.personLi}>Более 20-ти успешно завершенных проектов;</li>
+              <li className={p.personLi}>Полный список навыков на ЯП Javascript...</li>
+            </div>
+            <div className={p.personText}>
+              <p>Зарегистрирован плательщиком налога на профессиональный доход в соответствии с Законом Республики Хакасия от 28.05.2020 № 16-ЗРХ «О введении в действие на территории Республики Хакасия специального налогового режима «Налог на профессиональный доход».</p>
+            </div>
+          </div>
+          <div className={p.personImage}></div>
+        </div>
+        <div className={classnames(a.advantages, 'fullScreen')}>
+          <div className={'boldHeader'}>
+            <h3>Почему меня выбирают исполнителем?</h3>
+          </div>
+          <div className={classnames(a.advList, 'column')}>
+            <div className={classnames(a.advItem, 'column')}>
+              <div className={classnames(a.advText, 'row')}>
+                <DoneIcon style={advIcon} color="error" />
+                <div className={classnames('boldHeader', 'start')}><h5>Ответственное отношение к взятым на себя обязательствам</h5></div>
+              </div>
+              <div className={'boldDesc'}>Если берусь за дело, значит результат гарантирован. Потому что, перед принятием условий, всегда проверяю техническую возможность их исполнения</div>
+            </div>
+            <div className={classnames(a.advItem, 'column')}>
+              <div className={classnames(a.advText, 'row')}>
+                <DoneIcon style={advIcon} color="error" />
+                <div className={classnames('boldHeader', 'start')}><h5>Высокий уровень самодисциплины</h5></div>
+              </div>
+              <div className={'boldDesc'}>Всё рабочее время посвящаю работе над проектом заказчика. Одновременно держу в разработке не более одного проекта</div>
+            </div>
+            <div className={classnames(a.advItem, 'column')}>
+              <div className={classnames(a.advText, 'row')}>
+                <DoneIcon style={advIcon} color="error" />
+                <div className={classnames('boldHeader', 'start')}><h5>Наличие всех необходимых знаний и навыков</h5></div>
+              </div>
+              <div className={'boldDesc'}>Так как всему привык обучаться самостоятельно. Новые знания в своей профессиональной сфере приобретаю легко и быстро</div>
+            </div>
+            <div className={classnames(a.advItem, 'column')}>
+              <div className={classnames(a.advText, 'row')}>
+                <DoneIcon style={advIcon} color="error" />
+                <div className={classnames('boldHeader', 'start')}><h5>Всегда на cвязи</h5></div>
+              </div>
+              <div className={'boldDesc'}>Даже после завершения проекта, в течении дня, нахожу время, чтобы ответить на все вопросы клиента</div>
             </div>
           </div>
         </div>
       </main>
     </div>
+    </ ThemeProvider>
   )
 }
