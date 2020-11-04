c0e87a31ba2de74693b5969ff2d05e27d6ef6e10
kolserdav <serega12101983@gmail.com>
2020-11-04 12:30:12 +0700

    404, sitemap, robots

---
+++ pages/[...404].tsx
@@ -0,0 +1,7 @@
+import classnames from 'classnames'
+
+export default function Page404() {
+  return (
+    <div className={classnames('vcenter', 'center', 'column')}><h1 className='gray'>Страница не найдена ¯\_(ツ)_/¯</h1></div>
+  );
+}
