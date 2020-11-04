b5540d4deba39f6a6bdfe7ffde23cc021529212c
kolserdav <serega12101983@gmail.com>
2020-11-04 13:36:29 +0700

    Fix status 404

--- pages/[...404].tsx
+++ pages/[...404].tsx
@@ -1,7 +1,19 @@
 import classnames from 'classnames'
+import { GetServerSidePropsContext } from 'next'
+
+export function getServerSideProps(context: GetServerSidePropsContext) {
+  context.res.statusCode = 404;
+  return {
+    props: {
+      someProp: 'some_value'
+    }
+  };
+}
 
 export default function Page404() {
   return (
-    <div className={classnames('vcenter', 'center', 'column')}><h1 className='gray'>Страница не найдена ¯\_(ツ)_/¯</h1></div>
+    <div className={classnames('vcenter', 'center', 'column')}>
+      <h1 className='gray'>Страница не найдена ¯\_(ツ)_/¯</h1>
+    </div>
   );
 }
